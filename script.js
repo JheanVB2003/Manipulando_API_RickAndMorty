37f833ecb4dd050781d3ff337db140afdb297d68
Jhean Victo Barbosa Almeida <jheanbarbosa1233@gmail.com>
2023-09-27 15:50:39 -0300

    Consumindo a API Rick and Morty e filtrando informações

---
+++ script.js
@@ -0,0 +1,35 @@
+const characterId = document.getElementById('characterId');
+const btnGo = document.getElementById('btn-go');
+const content = document.getElementById('content');
+const image = document.getElementById('img');
+
+const fetchApi = (value) => {
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+    console.log(data); 
+    return data;
+    });
+
+     return result;
+}
+
+const keys = ['name', 'status', 'species', 'gender', 'origin',
+'image', 'episode'];
+
+const buildResult = (result) =>{
+    const newObject = {};
+    keys.map((key) => document.getElementById(key))
+        .map((elem) => {
+            elem.checked && (newObject[elem.name] = result[elem.name]);
+        })
+    return newObject;
+}
+
+btnGo.addEventListener('click', async (event) =>{
+    event.preventDefault();
+    const result = await fetchApi(characterId.value);
+    //content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
+    image.src = `${result.image}`;
+});
